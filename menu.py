2c212fe00e77541ac861095749d9a3d6e93f207f
rizwanadam <riam16cs@cmrit.ac.in>
2017-10-01 17:36:43 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 class menu:
-    d={}
+    def __init__(self):
+        self.d={}
     def show(self):
         print("\tMENU\n")
         for i in self.d:
@@ -12,7 +13,8 @@ for i in range(1000):
     st1=str(input("Enter food item:"))
     p1=int(input("Enter cost:"))
     c.add(st1,p1)
-    ch=input("Do you wish to continue adding(y/n):")
+    ch=str(input("Do you wish to continue adding(y/n):"))
     if(ch=='n'):
         break
 c.show()
+
