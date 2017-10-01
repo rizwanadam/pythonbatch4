a0d224eefc20d2f1d40c4f064f5c8ef4dfbb0d06
rizwanadam <riam16cs@cmrit.ac.in>
2017-10-01 15:59:04 +0530

    Update email.py

--- email.py
+++ email.py
@@ -3,7 +3,7 @@ import getpass
 mail=smtplib.SMTP('smtp.gmail.com',587)
 mail.ehlo()
 mail.starttls()
-txt=str(raw_input("Enter text to be sent:\n"))
+txt=raw_input("Enter text to be sent:\n")
 sdr=raw_input("Enter your email ID:")
 rcr=raw_input("Enter reciever's email ID:")
 pwd=getpass.getpass('Enter password:')
