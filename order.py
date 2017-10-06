143b3b2704f276a10684247bb507b77aceae164b
rizwanadam <riam16cs@cmrit.ac.in>
2017-10-06 21:24:24 +0530

    Ordering from menu

---
+++ order.py
@@ -0,0 +1,48 @@
+class menu:
+    def __init__(self):
+        self.d={}
+    def show(self):
+        print("\tMENU\n")
+        for i in self.d:
+            print(i," ",self.d[i])
+    def add(self,st,p):
+        self.d[st]=p
+class order():
+    def __init__(self,me):
+        self.d1={}
+        self.d=me.d
+    def placeo(self,st1,qt):
+        f=0
+        for i in self.d:
+            if(st1==i):
+                self.d1[st1]=qt
+                f=1
+                break
+        if(f!=1):
+            print("Item not present")
+    def disporder(self):
+        tot=0
+        print("Order\tQuantity\tPrice")
+        for i in self.d1:
+            print(i,"\t",self.d1[i],"\t\t",self.d1[i]*self.d[i])
+            tot+=self.d1[i]*self.d[i]
+        print("\nThe total cost is ",tot)
+c=menu()
+c.add("Idly",20)
+for i in range(1000):
+    st1=str(input("Enter food item:"))
+    p1=int(input("Enter cost:"))
+    c.add(st1,p1)
+    ch=str(input("Do you wish to continue adding(y/n):"))
+    if(ch=='n'):
+        break
+c.show()
+o=order(c)
+for i in range(100):
+    s=str(input("Enter order:"))
+    q=int(input("Enter quantity:"))
+    o.placeo(s,q)
+    c1=str(input("Do you wish to order more(y/n):"))
+    if(c1=='n'):
+        break
+o.disporder()
